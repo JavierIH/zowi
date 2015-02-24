8f8eef776a2c6b0fd1e090d7a0d73ab612f54e27
JavierIH <javier.isabel@bq.com>
2015-02-24 14:45:29 +0100

    New dancing movements!

--- code/zum/smooth_criminal/smooth_criminal.ino
+++ code/zum/smooth_criminal/smooth_criminal.ino
@@ -14,7 +14,10 @@
 #define PIN_YL 6
 
 Oscillator osc[N_OSCILLATORS];
+Servo servo;
 
+void kickLeft(int tempo);
+void kickRight(int tempo);
 void run(int steps, int T=500);
 void walk(int steps, int T=1000);
 void backyard(int steps, int T=3000);
@@ -34,11 +37,29 @@ void setup()
   osc[1].attach(PIN_RL);
   osc[2].attach(PIN_YR);
   osc[3].attach(PIN_YL);
+  
+  osc[0].SetTrim(TRIM_RR);
+  osc[1].SetTrim(TRIM_RL);
+  osc[2].SetTrim(TRIM_YR);
+  osc[3].SetTrim(TRIM_YL);
 }
+
+int t=1000;
 
 void loop()
 {
-  walk(1,1000);
+  
+  //kickLeft(t/2);
+  //kickRight(t/2);
+  //crusaito(1.5,t);
+  //upDown(1,t);
+  
+  for(int i=500; i<1000; i+=20)walk(1,i);
+  for(int i=1000; i>500; i-=20)walk(1,i);
+
+
+  
+
 }
 
 
@@ -58,9 +79,55 @@ void oscillate(int A[N_OSCILLATORS], int O[N_OSCILLATORS], int T, double phase_d
 }
 
 
+void kickLeft(int tempo){
+for(int i=0;i<4;i++) osc[i].SetPosition(90);
+  delay(tempo);
+  osc[0].SetPosition(50); //pie derecho
+  osc[1].SetPosition(70); //pie izquiero
+  delay(tempo);
+  osc[0].SetPosition(80); //pie derecho
+  osc[1].SetPosition(70); //pie izquiero
+  delay(tempo/4);
+  osc[0].SetPosition(30); //pie derecho
+  osc[1].SetPosition(70); //pie izquiero
+  delay(tempo/4);
+  osc[0].SetPosition(80); //pie derecho
+  osc[1].SetPosition(70); //pie izquiero
+  delay(tempo/4);
+  osc[0].SetPosition(30); //pie derecho
+  osc[1].SetPosition(70); //pie izquiero
+  delay(tempo/4);
+  osc[0].SetPosition(80); //pie derecho
+  osc[1].SetPosition(70); //pie izquiero
+  delay(tempo);
+}
+
+void kickRight(int tempo){
+for(int i=0;i<4;i++) osc[i].SetPosition(90);
+  delay(tempo);
+  osc[0].SetPosition(110); //pie derecho
+  osc[1].SetPosition(130); //pie izquiero
+  delay(tempo);
+  osc[0].SetPosition(110); //pie derecho
+  osc[1].SetPosition(100); //pie izquiero
+  delay(tempo/4);
+  osc[0].SetPosition(110); //pie derecho
+  osc[1].SetPosition(150); //pie izquiero
+  delay(tempo/4);
+  osc[0].SetPosition(110); //pie derecho
+  osc[1].SetPosition(80); //pie izquiero
+  delay(tempo/4);
+  osc[0].SetPosition(110); //pie derecho
+  osc[1].SetPosition(150); //pie izquiero
+  delay(tempo/4);
+  osc[0].SetPosition(110); //pie derecho
+  osc[1].SetPosition(80); //pie izquiero
+  delay(tempo);
+}
+
 void walk(int steps, int T){
     int A[4]= {15, 15, 30, 30};
-    int O[4] = {TRIM_RR, TRIM_RL, TRIM_YR, TRIM_YL};
+    int O[4] = {0, 0, 0, 0};
     double phase_diff[4] = {DEG2RAD(0), DEG2RAD(0), DEG2RAD(90), DEG2RAD(90)};
     
     for(int i=0;i<steps;i++)oscillate(A,O, T, phase_diff);
@@ -68,7 +135,7 @@ void walk(int steps, int T){
 
 void run(int steps, int T){
     int A[4]= {10, 10, 10, 10};
-    int O[4] = {TRIM_RR, TRIM_RL, TRIM_YR, TRIM_YL};
+    int O[4] = {0, 0, 0, 0};
     double phase_diff[4] = {DEG2RAD(0), DEG2RAD(0), DEG2RAD(90), DEG2RAD(90)}; 
     
     for(int i=0;i<steps;i++)oscillate(A,O, T, phase_diff);
@@ -76,7 +143,7 @@ void run(int steps, int T){
 
 void backyard(int steps, int T){
     int A[4]= {15, 15, 30, 30};
-    int O[4] = {TRIM_RR, TRIM_RL, TRIM_YR, TRIM_YL};
+    int O[4] = {0, 0, 0, 0};
     double phase_diff[4] = {DEG2RAD(0), DEG2RAD(0), DEG2RAD(-90), DEG2RAD(-90)}; 
     
     for(int i=0;i<steps;i++)oscillate(A,O, T, phase_diff);
@@ -84,7 +151,7 @@ void backyard(int steps, int T){
 
 void backyardSlow(int steps, int T){
     int A[4]= {15, 15, 30, 30};
-    int O[4] = {TRIM_RR, TRIM_RL, TRIM_YR, TRIM_YL};
+    int O[4] = {0, 0, 0, 0};
     double phase_diff[4] = {DEG2RAD(0), DEG2RAD(0), DEG2RAD(-90), DEG2RAD(-90)}; 
     
     for(int i=0;i<steps;i++)oscillate(A,O, T, phase_diff);
@@ -93,7 +160,7 @@ void backyardSlow(int steps, int T){
 
 void turnLeft(int steps, int T){
     int A[4]= {20, 20, 10, 30};
-    int O[4] = {TRIM_RR, TRIM_RL, TRIM_YR, TRIM_YL};
+    int O[4] = {0, 0, 0, 0};
     double phase_diff[4] = {DEG2RAD(0), DEG2RAD(0), DEG2RAD(90), DEG2RAD(90)}; 
     
     for(int i=0;i<steps;i++)oscillate(A,O, T, phase_diff);
@@ -101,7 +168,7 @@ void turnLeft(int steps, int T){
 
 void turnRight(int steps, int T){
     int A[4]= {20, 20, 30, 10};
-    int O[4] = {TRIM_RR, TRIM_RL, TRIM_YR, TRIM_YL};
+    int O[4] = {0, 0, 0, 0};
     double phase_diff[4] = {DEG2RAD(0), DEG2RAD(0), DEG2RAD(90), DEG2RAD(90)}; 
     
     for(int i=0;i<steps;i++)oscillate(A,O, T, phase_diff);
@@ -109,7 +176,7 @@ void turnRight(int steps, int T){
 
 void moonWalkRight(int steps, int T){
     int A[4]= {25, 25, 0, 0};
-    int O[4] = {TRIM_RR - 15, TRIM_RL + 15, TRIM_YR, TRIM_YL};
+    int O[4] = {-15 ,15, 0, 0};
     double phase_diff[4] = {DEG2RAD(0), DEG2RAD(180 + 120), DEG2RAD(90), DEG2RAD(90)}; 
     
     for(int i=0;i<steps;i++)oscillate(A,O, T, phase_diff);
@@ -117,7 +184,7 @@ void moonWalkRight(int steps, int T){
 
 void moonWalkLeft(int steps, int T){
     int A[4]= {25, 25, 0, 0};
-    int O[4] = {TRIM_RR - 15, TRIM_RL + 15, TRIM_YR, TRIM_YL};
+    int O[4] = {-15, 15, 0, 0};
     double phase_diff[4] = {DEG2RAD(0), DEG2RAD(180 - 120), DEG2RAD(90), DEG2RAD(90)}; 
     
     for(int i=0;i<steps;i++)oscillate(A,O, T, phase_diff);
@@ -125,7 +192,7 @@ void moonWalkLeft(int steps, int T){
 
 void crusaito(int steps, int T){
     int A[4]= {25, 25, 30, 30};
-    int O[4] = {TRIM_RR - 15, TRIM_RL + 15, TRIM_YR, TRIM_YL};
+    int O[4] = {- 15, 15, 0, 0};
     double phase_diff[4] = {DEG2RAD(0), DEG2RAD(180 + 120), DEG2RAD(90), DEG2RAD(90)}; 
     
     for(int i=0;i<steps;i++)oscillate(A,O, T, phase_diff);
@@ -133,7 +200,7 @@ void crusaito(int steps, int T){
 
 void swing(int steps, int T){
     int A[4]= {25, 25, 0, 0};
-    int O[4] = {TRIM_RR - 15, TRIM_RL + 15, TRIM_YR, TRIM_YL};
+    int O[4] = {-15, 15, 0, 0};
     double phase_diff[4] = {DEG2RAD(0), DEG2RAD(0), DEG2RAD(90), DEG2RAD(90)};
     
     for(int i=0;i<steps;i++)oscillate(A,O, T, phase_diff);
@@ -141,15 +208,15 @@ void swing(int steps, int T){
 
 void upDown(int steps, int T){
     int A[4]= {25, 25, 0, 0};
-    int O[4] = {TRIM_RR - 15, TRIM_RL + 15, TRIM_YR, TRIM_YL};
-    double phase_diff[4] = {DEG2RAD(0), DEG2RAD(180), DEG2RAD(90), DEG2RAD(90)};
+    int O[4] = {-15, 15, 0, 0};
+    double phase_diff[4] = {DEG2RAD(180), DEG2RAD(0), DEG2RAD(270), DEG2RAD(270)};
     
     for(int i=0;i<steps;i++)oscillate(A,O, T, phase_diff);
 }
 
 void flapping(int steps, int T){
     int A[4]= {15, 15, 8, 8};
-    int O[4] = {TRIM_RR - A[0] + 10, TRIM_RL + A[1] - 10, TRIM_YR, TRIM_YL};
+    int O[4] = {-A[0] + 10, A[1] - 10, 0, 0};
     double phase_diff[4] = {DEG2RAD(0), DEG2RAD(180), DEG2RAD(90), DEG2RAD(-90)};
     
     for(int i=0;i<steps;i++)oscillate(A,O, T, phase_diff);
@@ -157,7 +224,7 @@ void flapping(int steps, int T){
 
 void test(int steps, int T){
     int A[4]= {15, 15, 8, 8};
-    int O[4] = {TRIM_RR - A[0] + 10, TRIM_RL + A[1] - 10, TRIM_YR, TRIM_YL};
+    int O[4] = {-A[0] + 10, A[1] - 10, 0, 0};
     double phase_diff[4] = {DEG2RAD(0), DEG2RAD(180), DEG2RAD(90), DEG2RAD(-90)};
     
     for(int i=0;i<steps;i++)oscillate(A,O, T, phase_diff);
