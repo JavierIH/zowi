a2edcee11d724e1a7fb99fa945b44ff9db1c858f
JavierIH <javier.isabel@bq.com>
2015-04-27 14:38:07 +0200

    Fixed code for Arduino IDE version > 1.5

--- code/zum/smooth_criminal/smooth_criminal.ino
+++ code/zum/smooth_criminal/smooth_criminal.ino
@@ -1,19 +1,22 @@
 #include <Servo.h>
 #include <Oscillator.h>
+#include <EEPROM.h>
 
 #define N_SERVOS 4
 
+#define EEPROM_TRIM 1 //Activar para coger los datos de calibracin desde la memoria interna
+
+#define TRIM_RR 0
+#define TRIM_RL 0
+#define TRIM_YR 0
+#define TRIM_YL 0
+
+#define PIN_RR 3
+#define PIN_RL 2 //8
+#define PIN_YR 5
+#define PIN_YL 4
+
 #define INTERVALTIME 10.0 
-
-#define TRIM_RR -10
-#define TRIM_RL -8
-#define TRIM_YR 8
-#define TRIM_YL 7
-
-#define PIN_RR 4
-#define PIN_RL 5 //8
-#define PIN_YR 2
-#define PIN_YL 3
 
 Oscillator servo[N_SERVOS];
 
@@ -44,14 +47,32 @@ void setup()
   servo[2].attach(PIN_YR);
   servo[3].attach(PIN_YL);
   
-  servo[0].SetTrim(TRIM_RR);
-  servo[1].SetTrim(TRIM_RL);
-  servo[2].SetTrim(TRIM_YR);
-  servo[3].SetTrim(TRIM_YL);
+  //EEPROM.write(0,TRIM_RR);
+  //EEPROM.write(1,TRIM_RL);
+  //EEPROM.write(2,TRIM_YR);
+  //EEPROM.write(3,TRIM_YL);
+  
+  int trim;
+  
+  if(EEPROM_TRIM){
+    for(int x=0;x<4;x++){
+      trim=EEPROM.read(x);
+      if(trim>128)trim=trim-256;
+      Serial.print("TRIM ");
+      Serial.print(x);
+      Serial.print(" en ");
+      Serial.println(trim);
+      servo[x].SetTrim(trim);
+    }
+  }
+  else{
+    servo[0].SetTrim(TRIM_RR);
+    servo[1].SetTrim(TRIM_RL);
+    servo[2].SetTrim(TRIM_YR);
+    servo[3].SetTrim(TRIM_YL);
+  }
   
   for(int i=0;i<4;i++) servo[i].SetPosition(90);
-
-  
 }
 
 // TEMPO: 121 BPM
@@ -60,13 +81,16 @@ double pause=0;
 
 void loop()
 {
-  if(Serial.available()){
-    char init = Serial.read();
-    if (init=='X'){
-      delay(4000); //3000 - 4500
+ // if(Serial.available()){
+  //  char init = Serial.read();
+   // if (init=='X'){
+   //   delay(4000); //3000 - 4500
+   
       dance();
-    }
-  }
+      
+          //for(int i=0;i<4;i++) servo[i].SetPosition(90);
+   // }
+ // }
 }
 
 void dance(){
@@ -76,7 +100,7 @@ void dance(){
   moonWalkRight(4,t*2);
   moonWalkLeft(4,t*2);
   moonWalkRight(4,t*2);
-  primera_parte(); //otra vez
+  primera_parte(); 
   crusaito(1,t*8);
   crusaito(1,t*7);
 
@@ -104,15 +128,15 @@ void dance(){
   drunk(t/2);
 
   delay(t*4); 
-  walk(2,t*2);//Javi del futuro sigue aqui
-  backyard(2,t*2);//Javi del futuro sigue aqui
+  walk(2,t*2);
+  backyard(2,t*2);
   goingUp(t*2);
   goingUp(t*1);
   noGravity(t*2);
   crusaito(1,t*2);
   crusaito(1,t*8);
   crusaito(1,t*2);
-  crusaito(1,t*8);/**/
+  crusaito(1,t*8);
   crusaito(1,t*2);
   crusaito(1,t*3);
 
@@ -124,16 +148,14 @@ void dance(){
   }
   
   for(int i=0;i<4;i++) servo[i].SetPosition(90);
-
-
 }
 
 
 
 
-//////////////////////////////////////////////////////////////////////////////
-/////////////////////////////FUNCIONES DE CONTROL/////////////////////////////
-//////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////FUNCIONES DE CONTROL//////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////
 
 void oscillate(int A[N_SERVOS], int O[N_SERVOS], int T, double phase_diff[N_SERVOS]){
   for (int i=0; i<4; i++) {
@@ -189,6 +211,7 @@ void moveNServos(int time, int  newPosition[]){
 //////////////////////////////////////////////////////////////////////////////
 
 void goingUp(int tempo){
+  
       pause=millis();
       for(int i=0;i<4;i++) servo[i].SetPosition(90);
       delay(tempo);
@@ -215,13 +238,15 @@ void goingUp(int tempo){
       delay(tempo);
       
       while(millis()<pause+8*t);
-      
-
-
 
 }
 
 void primera_parte(){
+  
+  int move1[4] = {60,120,90,90};
+  int move2[4] = {90,90,90,90};
+  int move3[4] = {40,140,90,90};
+  
   for(int x=0; x<3; x++){
     for(int i=0; i<3; i++){
       lateral_fuerte(1,t/2);
@@ -232,52 +257,66 @@ void primera_parte(){
   
     pause=millis();
     for(int i=0;i<4;i++) servo[i].SetPosition(90);
-    moveNServos(t*0.4,(int[]){60,120,90,90});
-    moveNServos(t*0.4,(int[]){90,90,90,90});
+    moveNServos(t*0.4,move1);
+    moveNServos(t*0.4,move2);
     while(millis()<(pause+t*2));
   }
   
-   for(int i=0; i<2; i++){
-      lateral_fuerte(1,t/2);
-      lateral_fuerte(0,t/4);
-      lateral_fuerte(1,t/4);
-      delay(t);
-    }
-  
-    pause=millis();
-    for(int i=0;i<4;i++) servo[i].SetPosition(90);
-    crusaito(1,t*1.4);
-    moveNServos(t*1,(int[]){40,140,90,90});
-    for(int i=0;i<4;i++) servo[i].SetPosition(90);
-    while(millis()<(pause+t*4));
- 
+  for(int i=0; i<2; i++){
+    lateral_fuerte(1,t/2);
+    lateral_fuerte(0,t/4);
+    lateral_fuerte(1,t/4);
+    delay(t);
+  }
+    
+  pause=millis();
+  for(int i=0;i<4;i++) servo[i].SetPosition(90);
+  crusaito(1,t*1.4);
+  moveNServos(t*1,move3);
+  for(int i=0;i<4;i++) servo[i].SetPosition(90);
+  while(millis()<(pause+t*4));
 }
 
 void segunda_parte(){
-  for(int x=0; x<7; x++){
-  for(int i=0; i<3; i++){
+  
+  int move1[4] = {90,90,80,100};
+  int move2[4] = {90,90,100,80};
+  int move3[4] = {90,90,80,100};
+  int move4[4] = {90,90,100,80};
+    
+  int move5[4] = {40,140,80,100};
+  int move6[4] = {40,140,100,80};
+  int move7[4] = {90,90,80,100};
+  int move8[4] = {90,90,100,80};
+       
+  int move9[4] = {40,140,80,100};
+  int move10[4] = {40,140,100,80};
+  int move11[4] = {90,90,80,100};
+  int move12[4] = {90,90,100,80};
+  
+  for(int x=0; x<7; x++){ 
+    for(int i=0; i<3; i++){
+      pause=millis();
+      moveNServos(t*0.15,move1);
+      moveNServos(t*0.15,move2);
+      moveNServos(t*0.15,move3);
+      moveNServos(t*0.15,move4);
+      while(millis()<(pause+t));
+    }
     pause=millis();
-    moveNServos(t*0.15,(int[]){90,90,80,100});
-    moveNServos(t*0.15,(int[]){90,90,100,80});
-    moveNServos(t*0.15,(int[]){90,90,80,100});
-    moveNServos(t*0.15 ,(int[]){90,90,100,80});
+    moveNServos(t*0.15,move5);
+    moveNServos(t*0.15,move6);
+    moveNServos(t*0.15,move7);
+    moveNServos(t*0.15,move8);
     while(millis()<(pause+t));
-  }
-  pause=millis();
-  moveNServos(t*0.15,(int[]){40,140,80,100});
-  moveNServos(t*0.15,(int[]){40,140,100,80});
-  moveNServos(t*0.15,(int[]){90,90,80,100});
-  moveNServos(t*0.15 ,(int[]){90,90,100,80});
-  while(millis()<(pause+t));
   }
  
   for(int i=0; i<3; i++){
-
     pause=millis();
-    moveNServos(t*0.15,(int[]){40,140,80,100});
-    moveNServos(t*0.15,(int[]){40,140,100,80});
-    moveNServos(t*0.15,(int[]){90,90,80,100});
-    moveNServos(t*0.15 ,(int[]){90,90,100,80});
+    moveNServos(t*0.15,move9);
+    moveNServos(t*0.15,move10);
+    moveNServos(t*0.15,move11);
+    moveNServos(t*0.15,move12);
     while(millis()<(pause+t));
   }
 }
@@ -295,31 +334,45 @@ void lateral_fuerte(boolean side, int tempo){
 }
 
 void drunk (int tempo){
+  
   pause=millis();
-  moveNServos(tempo*0.235,(int[]){60,70,90,90});
-  moveNServos(tempo*0.235,(int[]){110,120,90,90});
-  moveNServos(tempo*0.235,(int[]){60,70,90,90});
-  moveNServos(tempo*0.235,(int[]){110,120,90,90});
+  
+  int move1[] = {60,70,90,90};
+  int move2[] = {110,120,90,90};
+  int move3[] = {60,70,90,90};
+  int move4[] = {110,120,90,90};
+  
+  moveNServos(tempo*0.235,move1);
+  moveNServos(tempo*0.235,move2);
+  moveNServos(tempo*0.235,move3);
+  moveNServos(tempo*0.235,move4);
   while(millis()<(pause+tempo));
 
 }
 
 
 void noGravity(int tempo){
+  
+  int move1[4] = {120,140,90,90};
+  int move2[4] = {140,140,90,90};
+  int move3[4] = {120,140,90,90};
+  int move4[4] = {90,90,90,90};
+  
+  
   for(int i=0;i<4;i++) servo[i].SetPosition(90);
   for(int i=0;i<N_SERVOS;i++) oldPosition[i]=90;
-  moveNServos(tempo*2,(int[]){120,140,90,90});
-  moveNServos(tempo*2,(int[]){140,140,90,90});
+  moveNServos(tempo*2,move1);
+  moveNServos(tempo*2,move2);
   delay(tempo*2);
-  moveNServos(tempo*2,(int[]){120,140,90,90});
-  moveNServos(tempo*2,(int[]){90,90,90,90});
+  moveNServos(tempo*2,move3);
+  moveNServos(tempo*2,move4);
 
  
 
 }
 
 void kickLeft(int tempo){
-for(int i=0;i<4;i++) servo[i].SetPosition(90);
+  for(int i=0;i<4;i++) servo[i].SetPosition(90);
   delay(tempo);
   servo[0].SetPosition(50); //pie derecho
   servo[1].SetPosition(70); //pie izquiero
