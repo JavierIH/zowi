dd355bac467647093183af896f96c4fb42274e0b
obijuan <juan@iearobotics.com>
2015-07-28 16:33:15 +0200

    Example that shows all the locomotion gaits implemented on zowi: walk, turn, updown, swing, moonwalker, crusaito, flapping

--- code/zum/libraries/Zowi/Zowi.cpp
+++ code/zum/libraries/Zowi/Zowi.cpp
@@ -1,3 +1,11 @@
+#if defined(ARDUINO) && ARDUINO >= 100
+  #include "Arduino.h"
+#else
+  #include "WProgram.h"
+  #include <pins_arduino.h>
+#endif
+
+
 #include "Zowi.h"
 #include <Oscillator.h>
 
@@ -54,56 +62,191 @@ void Zowi::oscillateServos(int A[4], int O[4], int T, double phase_diff[4], floa
     servo[i].SetPh(phase_diff[i]);
   }
   double ref=millis();
-   for (double x=ref; x<T*cycle+ref; x=millis()){
+   for (double x=ref; x<=T*cycle+ref; x=millis()){
      for (int i=0; i<4; i++){
         servo[i].refresh();
      }
   }
 }
 
+//--------------------------------
+//-- Zowi at rest position
+//--------------------------------
+void Zowi::home()
+{
+  //-- All the parameters are set to 0
+  //-- If the amplitudes are 0, there are no oscillation
+  int A[4]= {0, 0, 0, 0};
+  int O[4] = {0, 0, 0, 0};
+  double phase_diff[4] = {0, 0, 0, 0};
+  
+  //-- Let's update the oscillators parameters!
+  oscillateServos(A,O,500,phase_diff,1);
+}
 
-void Zowi::walk(float steps, int T){
+void Zowi::_execute(int A[4], int O[4], int T, double phase_diff[4], float steps = 1.0)
+{
+  int cycles=(int)steps;    
+
+  //-- Execute complete cycles
+  if (cycles >= 1) 
+    for(int i = 0; i < cycles; i++) 
+      oscillateServos(A,O, T, phase_diff);
+      
+  //-- Execute the final not complete cycle    
+  oscillateServos(A,O, T, phase_diff,(float)steps-cycles);
+}
+
+
+//------------------------------------------------
+//-- Zowi gait: Walking  (forward or backward)    
+//-- Parameters:
+//--    * steps:  Number of steps
+//--    * T : Period
+//--    * Dir: Direction: FORWARD / BACKWARD
+//------------------------------------------------
+void Zowi::walk(float steps, int T, int dir)
+{
+  //-- Oscillator parameters for walking
+  //-- Hip sevos are in phase
+  //-- Feet servos are in phase
+  //-- Hip and feet are 90 degrees out of phase
+  //--      -90 : Walk forward
+  //--       90 : Walk backward
+  //-- Feet servos also have the same offset (for tiptoe a little bit)
   int A[4]= {30, 30, 20, 20};
-  int O[4] = {0, 0, 0, 0};
-  double phase_diff[4] = {DEG2RAD(180), DEG2RAD(180), DEG2RAD(90), DEG2RAD(90)};
+  int O[4] = {0, 0, 4, -4};
+  double phase_diff[4] = {0, 0, DEG2RAD(dir * -90), DEG2RAD(dir * -90)};
 
-  int cycles=(int)steps;    
-
-  if (cycles >= 1) for(int i=0;i<cycles;i++) oscillateServos(A,O, T, phase_diff);
-  oscillateServos(A,O, T, phase_diff,(float)steps-cycles);
+  //-- Let's oscillate the servos!
+  _execute(A, O, T, phase_diff, steps);  
 }
 
-void Zowi::backward(float steps, int T){
-  int A[4]= {15, 15, 30, 30};
-  int O[4] = {0, 0, 0, 0};
-  double phase_diff[4] = {DEG2RAD(180), DEG2RAD(180), DEG2RAD(270), DEG2RAD(270)}; 
+//----------------------------------------------------
+//-- Zowi gait: Turning (left or right)
+//-- Parameters:
+//--   * Steps: Number of steps
+//--   * T: Period
+//--   * Dir: Direction: LEFT / RIGHT
+//-----------------------------------------------------
+void Zowi::turn(float steps, int T, int dir)
+{
+  //-- Same coordination than for walking (see Zowi::walk)
+  //-- The Amplitudes of the hip's oscillators are not igual
+  //-- When the right hip servo amplitude is higher, the steps taken by
+  //--   the right leg are bigger than the left. So, the robot describes an 
+  //--   left arc
+  int A[4]= {30, 30, 20, 20};
+  int O[4] = {0, 0, 4, -4};
+  double phase_diff[4] = {0, 0, DEG2RAD(-90), DEG2RAD(-90)}; 
     
-  int cycles=(int)steps;    
-
-  if (cycles >= 1) for(int i=0;i<cycles;i++) oscillateServos(A,O, T, phase_diff);
-  oscillateServos(A,O, T, phase_diff,(float)steps-cycles);
+  if (dir == LEFT) {  
+    A[0] = 30; //-- Left hip servo
+    A[1] = 10; //-- Right hip servo
+  }
+  else {
+    A[0] = 10;
+    A[1] = 30;
+  }
+    
+  //-- Let's oscillate the servos!
+  _execute(A, O, T, phase_diff, steps); 
 }
 
-void Zowi::turnLeft(float steps, int T){
-  int A[4]= {10, 10, 25, 25};
-  int O[4] = {0, 0, 0, 0};
-  double phase_diff[4] = {DEG2RAD(0), DEG2RAD(180), DEG2RAD(90), DEG2RAD(90)}; 
-    
-  int cycles=(int)steps;    
 
-  if (cycles >= 1) for(int i=0;i<cycles;i++) oscillateServos(A,O, T, phase_diff);
-  oscillateServos(A,O, T, phase_diff,(float)steps-cycles);
+//------------------------------------------------------
+//-- Zowi movement: up-down
+//--   Zowi moves up and down
+//--
+//-- Parameters:
+//--    * steps: Number of jumps
+//--    * T: Period
+//--    * h: Jump height: SMALL / MEDIUM / BIG 
+//--              (or a number in degrees 0 - 90)
+//----------------------------------------------------- 
+void Zowi::updown(float steps, int T, int h)
+{
+  //-- Both feet are 180 degrees out of phase
+  //-- Feet amplitude and offset are the same
+  //-- Initial phase for the right foot is -90, so that it starts
+  //--   in one extreme position (not in the middle)
+  int A[4]= {0, 0, h, h};
+  int O[4] = {0, 0, h, -h};
+  double phase_diff[4] = {0, 0, DEG2RAD(-90), DEG2RAD(90)};
+  
+  //-- Let's oscillate the servos!
+  _execute(A, O, T, phase_diff, steps); 
 }
 
-void Zowi::turnRight(float steps, int T){
-  int A[4]= {20, 20, 30, 10};
-  int O[4] = {0, 0, 0, 0};
-  double phase_diff[4] = {DEG2RAD(0), DEG2RAD(0), DEG2RAD(90), DEG2RAD(90)}; 
-    
-  int cycles=(int)steps;    
+//-----------------------------------------------------------------
+//-- Zowi gait: Moonwalker
+//--   Zowi moves like Michael Jackson
+//--
+//--  Parameters:
+//--    Steps: Number of steps
+//--    T: Period
+//--    h: Height. Typical valures between 15 and 40
+//--    dir: Direction: LEFT / RIGHT
+//------------------------------------------------------------------
+void Zowi::moonwalker(float steps, int T, int h, int dir)
+{
+  //-- This motion is similar to that of the caterpillar robots: A travelling
+  //-- wave moving from one side to another
+  //-- The two Zowi's feet are equivalent to a minimal configuration. It is known
+  //-- that 2 servos can move like a worm if they are 120 degrees out of phase
+  //-- In the example of Zowi, the two feet are mirrored so that we have:
+  //--    180 - 120 = 60 degrees. The actual phase difference given to the oscillators
+  //--  is 60 degrees.
+  //--  Both amplitudes are equal. The offset is half the amplitud plus a little bit of
+  //-   offset so that the robot tiptoe lightly
+ 
+  int A[4]= {0, 0, h, h};
+  int O[4] = {0, 0, h/2+2, -h/2 -2};
+  int phi = -dir * 90;
+  double phase_diff[4] = {0, 0, DEG2RAD(phi), DEG2RAD(-60 * dir + phi)};
+  
+  //-- Let's oscillate the servos!
+  _execute(A, O, T, phase_diff, steps); 
+}
 
-  if (cycles >= 1) for(int i=0;i<cycles;i++) oscillateServos(A,O, T, phase_diff);
-  oscillateServos(A,O, T, phase_diff,(float)steps-cycles);  
+//---------------------------------------------------------
+//-- Zowi movement: swinging side to side
+//--
+//--  Parameters:
+//--     steps: Number of steps
+//--     T : Period
+//--     h : Amount of swing (from 0 to 50 aprox)
+//-----------------------------------------------------------
+void Zowi::swing(float steps, int T, int h)
+{
+
+  //-- Both feets are in phase. The offset is half the amplitude
+  //-- It causes the robot to swing from side to side
+  int A[4]= {0, 0, h, h};
+  int O[4] = {0, 0, h/2, -h/2};
+  double phase_diff[4] = {0, 0, DEG2RAD(0), DEG2RAD(0)};
+  
+  //-- Let's oscillate the servos!
+  _execute(A, O, T, phase_diff, steps); 
+}
+
+//-----------------------------------------------------------------
+//-- Zowi gait: Crusaito: a mixture between moonwalker and walk
+//--
+//--   Parameters:
+//--     steps: Number of steps
+//--     T: Period
+//--     h: height (Values between 20 - 50)
+//--     dir:  Direction: LEFT / RIGHT
+//------------------------------------------------------------------
+void Zowi::crusaito(float steps, int T, int h, int dir)
+{
+  int A[4]= {25, 25, h, h};
+  int O[4] = {0, 0, h/2+ 4, -h/2 - 4};
+  double phase_diff[4] = {90, 90, DEG2RAD(0), DEG2RAD(-60 * dir)};
+  
+  //-- Let's oscillate the servos!
+  _execute(A, O, T, phase_diff, steps); 
 }
 
 void Zowi::jump(float steps, int T){
@@ -112,3 +255,27 @@ void Zowi::jump(float steps, int T){
   int down[]={90,90,90,90};
   moveServos(T,down);
 }
+
+//-----------------------------------------------------
+//-- Zowi gait: Flapping
+//--
+//--  Parameters:
+//--    steps: Number of steps
+//--    T: Period
+//--    h: height (Values between 10 - 30)
+//--    dir: direction: FOREWARD, BACKWARD
+//------------------------------------------------------
+void Zowi::flapping(float steps, int T, int h, int dir)
+{
+  int A[4]= {12, 12, h, h};
+  int O[4] = {0, 0, h - 10, -h + 10};
+  double phase_diff[4] = {DEG2RAD(0), DEG2RAD(180), DEG2RAD(-90 * dir), DEG2RAD(90 * dir)};
+  
+  //-- Let's oscillate the servos!
+  _execute(A, O, T, phase_diff, steps); 
+}
+
+
+
+
+
--- code/zum/libraries/Zowi/Zowi.h
+++ code/zum/libraries/Zowi/Zowi.h
@@ -1,13 +1,18 @@
 #ifndef Zowi_h
 #define Zowi_h
 
-
-#include "Arduino.h"
 #include <Servo.h>
 #include <Oscillator.h>
 #include <EEPROM.h>
 
-
+//-- Constants
+#define FORWARD   1
+#define BACKWARD -1
+#define LEFT      1
+#define RIGHT    -1
+#define SMALL 5
+#define MEDIUM 15
+#define BIG 30
 
 class Zowi
 {
@@ -19,11 +24,16 @@ class Zowi
     void moveServos(int time, int  servo_target[]);
     void oscillateServos(int A[4], int O[4], int T, double phase_diff[4], float cycle);
     
-    void walk(float steps=4, int T=1800);
-    void turnLeft(float steps, int T);
-    void turnRight(float steps, int T);
-    void backward(float steps, int T);
-    void jump(float steps=1, int T=1000);
+    void home();
+    void walk(float steps=4, int T=1800, int dir = FORWARD);
+    void turn(float steps=1, int T=2000, int dir = LEFT);
+    void updown(float steps=1, int T=1000, int h = 20);
+    void moonwalker(float steps, int T, int h, int dir);
+    void swing(float steps, int T, int h);
+    void crusaito(float steps, int T, int h, int dir);
+    void flapping(float steps, int T, int h, int dir);
+    void jump(float steps=1, int T = 8000);
+    
 
  
   private:
@@ -35,6 +45,8 @@ class Zowi
     unsigned long final_time;
     unsigned long partial_time;
     float increment[4];
+    
+    void _execute(int A[4], int O[4], int T, double phase_diff[4], float steps);
 };
 
 #endif
